e0edae4cc34cc1d115b2d46912f409ee074e8f42
alfanfauzy <afauzy@pvg.co.id>
2023-02-14 10:49:25 +0700

    feat(loading bar): add loading bar for all page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-file no-use-before-define
-import { ReactElement, ReactNode, Suspense, useEffect } from 'react'
+import { ReactElement, ReactNode, Suspense, useEffect, useState } from 'react'
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
@@ -8,6 +8,7 @@ import 'posy-fnb-core/dist/index.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import { dummy } from 'src/data'
+import LoadingBar from '@/atoms/loadingBar'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -21,6 +22,11 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const router = useRouter()
   const { asPath } = router
 
+  const [state, setState] = useState({
+    isRouteChanging: false,
+    loadingKey: 0,
+  })
+
   useEffect(() => {
     if (asPath === '/') {
       router.push('/auth/login')
@@ -28,12 +34,44 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
     localStorage.setItem('items', JSON.stringify(dummy))
   })
 
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: true,
+        // eslint-disable-next-line no-bitwise
+        loadingKey: prevState.loadingKey ^ 1,
+      }))
+    }
+
+    const handleRouteChangeEnd = () => {
+      setState((prevState) => ({
+        ...prevState,
+        isRouteChanging: false,
+      }))
+    }
+
+    router.events.on('routeChangeStart', handleRouteChangeStart)
+    router.events.on('routeChangeComplete', handleRouteChangeEnd)
+    router.events.on('routeChangeError', handleRouteChangeEnd)
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart)
+      router.events.off('routeChangeComplete', handleRouteChangeEnd)
+      router.events.off('routeChangeError', handleRouteChangeEnd)
+    }
+  }, [router.events])
+
   const getLayout =
     Component.getLayout ??
     ((page) => <Suspense fallback={<p>Loading . . . .</p>}>{page}</Suspense>)
 
   return getLayout(
     <>
+      <LoadingBar
+        isRouteChanging={state.isRouteChanging}
+        key={state.loadingKey}
+      />
       <Component {...pageProps} />
       <ToastContainer
         position="top-center"
