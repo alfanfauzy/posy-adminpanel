fc2debc74dc4f48b0543c87ce91b476a0c01b9bd
afauzy <afauzy@pvg.co.id>
2023-04-26 15:04:11 +0700

    fix(header): fix header avatar

--- src/components/templates/header/index.tsx
+++ src/components/templates/header/index.tsx
@@ -1,44 +1,58 @@
-import {Avatar, Badge, Dropdown, MenuProps} from 'antd';
+import {GenerateAvatar} from '@/constants/utils';
+import {LogoutPayload} from '@/domain/auth/models';
+import {useLogoutViewModal} from '@/view/auth/view-models/LogoutViewModel';
 import {useRouter} from 'next/router';
 import React from 'react';
 import {AiOutlineLogout} from 'react-icons/ai';
-import {useDispatchApp} from 'store/hooks';
+import {toast} from 'react-toastify';
+import {useAppSelector, useDispatchApp} from 'store/hooks';
 import {onLogout} from 'store/slice/auth';
 
 const TemplatesHeader = () => {
 	const router = useRouter();
 	const dispatch = useDispatchApp();
 
-	const handleLogout = () => {
-		dispatch(onLogout());
-		router.push('/auth/login');
+	const {user_info, token} = useAppSelector(state => state.auth.authData);
+
+	const {email, fullname, user_uuid} = user_info;
+
+	const payloadLogout: LogoutPayload = {
+		user_uuid,
+		token,
 	};
 
-	const items: MenuProps['items'] = [
-		{
-			label: (
-				<span
-					tabIndex={0}
-					role="button"
-					className="text-m-reguler flex items-center justify-center gap-1 rounded-lg border border-gray-200 bg-red-400 p-2 text-white"
-					onClick={() => handleLogout()}
-					onKeyDown={() => handleLogout()}
-				>
-					<AiOutlineLogout /> Logout
-				</span>
-			),
-			key: '0',
+	// const logout = () => {
+	// 	dispatch(onLogout());
+	// 	router.push('/auth/login');
+	// };
+
+	const {logout} = useLogoutViewModal({
+		onSuccess: data => {
+			if (data.code === 0) {
+				dispatch(onLogout());
+				router.push('/auth/login');
+				toast.success('Success Logout');
+			}
 		},
-	];
+	});
+
 	return (
-		<header className="flex w-full justify-end bg-white p-2 drop-shadow-lg">
-			<Dropdown menu={{items}} trigger={['hover']} className="w-12">
-				<a role="button" tabIndex={0} onKeyDown={e => e.preventDefault()}>
-					<Badge>
-						<Avatar size="large" src="https://i.pravatar.cc/30" />
-					</Badge>
-				</a>
-			</Dropdown>
+		<header className="flex w-full items-center justify-end gap-2 bg-white p-2 drop-shadow-lg">
+			<p className="text-s-regular">{email}</p>
+			<div className="border-r-2 border-gray-300 px-1">
+				<section className="w-50 flex items-center gap-2 rounded-full bg-[#01B89D] p-2 text-white">
+					<p>{GenerateAvatar(fullname)}</p>
+				</section>
+			</div>
+			<span
+				tabIndex={0}
+				role="button"
+				className="flex items-center justify-center gap-1 rounded-lg border border-gray-200 bg-red-400 p-2 text-xs text-white hover:border-red-200 hover:bg-red-300 hover:text-white"
+				onClick={() => logout(payloadLogout)}
+				onKeyDown={() => logout(payloadLogout)}
+			>
+				<AiOutlineLogout /> Logout
+			</span>
 		</header>
 	);
 };
